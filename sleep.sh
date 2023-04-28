29624058379c114a512b919a854d9a53c7218502
Bamhammed METEHRI <42822493+mozaBit@users.noreply.github.com>
2023-04-28 22:59:54 +0200

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -3,7 +3,7 @@
 BOOT_MIN=$(uptime -s | cut -d ":" -f 2)
 BOOT_SEC=$(uptime -s | cut -d ":" -f 3)
 
-DELAY=$(bc <<< $BOOT_MIN%10*60+$BOOT_SEC)
+DELAY=$(bc <<< $BOOT_MIN%10*60+$BOOT_SEC) #Vous devez installer la commande bc"sudo apt install bc"
 
 # Attendre ce nombre de minutes
 sleep $DELAY
